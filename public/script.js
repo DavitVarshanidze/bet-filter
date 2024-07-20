65735a837a1b6babe81b0c1d9cc1a9e0ff4cbeba
dvarshanidze <davittvarshanidze@gmail.com>
2024-07-20 21:13:10 +0400

    changed back to http

--- public/script.js
+++ public/script.js
@@ -18,9 +18,9 @@ async function fetchTotalizatorOdds(game) {
   }
 
   const totalizatorAPIs = [
-    `${window.env.API_URL}/totalizator1`,
-    `${window.env.API_URL}/totalizator2`,
-    `${window.env.API_URL}/totalizator3`,
+    `http://localhost:3000/totalizator1`,
+    `http://localhost:3000/totalizator2`,
+    `http://localhost:3000/totalizator3`,
   ];
 
   const totalizatorNames = ["Crystalbet", "Betlive", "Crocobet"];
